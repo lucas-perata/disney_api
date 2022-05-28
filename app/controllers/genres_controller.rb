27dd45a7dfa10ff6a5dded138cf7dad1dbbb7b85
lucas-perata <contacto.perata@gmail.com>
2022-05-28 14:40:10 -0300

    Agregado creación genres por medio de API

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,4 +5,21 @@ class GenresController < ApplicationController
         render json: @genres
     end 
 
+    def create 
+        @genre = Genre.new(genre_params)
+
+        if @genre.save
+            render json: @genre, status: :created
+          else
+            render json: @genre.errors, status: :unprocessable_entity
+          end
+
+    end     
+
+    private 
+
+    def genre_params 
+        params.require(:genre).permit(:name, :img_url)
+    end 
+
 end 
