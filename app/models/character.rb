47824f5c87f3974088a3bf58fa006a38e3cc29e0
lucas-perata <contacto.perata@gmail.com>
2022-05-28 22:22:43 -0300

    validaciones

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ApplicationRecord
+    validates :name, presence: true, uniqueness: true 
+    validates :img_url, presence: true
+    
     has_many :character_movies, dependent: :destroy
     has_many :movies, through: :character_movies, dependent: :destroy
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true 
+  validates :img_url, presence: true 
+
   has_many :character_movies, dependent: :destroy
   has_many :characters, through: :character_movies, dependent: :destroy
 end
